3af2020552050229eade806d81c6b22ef13f27c1
hiromi0226 <hiromi.26ha@gmail.com>
2018-07-26 20:43:39 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,9 @@
-# Code your instances here
+class GardenGnome 
+  attr_accessor :name, :age, :gluten_allergy 
+  
+  def initialize (personality = "evil", hat_color = "red")
+    @hat_color = hat_color  
+    @personality = personality
+  end 
+  
+  
